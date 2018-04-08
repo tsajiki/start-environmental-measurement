11d485425517c2b8fc6b4d8031c1663920bc5996
Tatsuo Sajiki <tsajiki@outlook.jp>
2018-04-08 21:36:37 +0900

    Update twelite_read.py

--- twelite_read.py
+++ twelite_read.py
@@ -6,7 +6,7 @@ from sys import exit
 import csv
 import datetime
 
-# シリアルポートを開く（"COM5"の場合）
+# open serial port: "COM5" etc
 try:
     ser = Serial("COM5", 115200)
     print("open serial port: %s" % "COM5")
@@ -14,24 +14,21 @@ except:
     print("cannot open serial port: %s" % "COM5")
     exit(1)
 
-# データを１行ずつ解釈する
 while True:
     now = datetime.datetime.now()
-    now=now.isoformat()
+    today = now.date()
+    now = now.isoformat()
 
-    date = datetime.datetime.today()
-    date=date.isoformat()
-
-    line = ser.readline().rstrip() # １ライン単位で読み出し、末尾の改行コードを削除（ブロッキング読み出し）
-
-    strLine=line.decode()
-    arrLine=strLine.split(";")
-    arrLine[0]=now
-
+    line = ser.readline().rstrip()
+    strLine = line.decode()
+    arrLine = strLine.split(";")
+    arrLine[0] = now
     print(arrLine)
-    if len(arrLine) > 4 and (arrLine[11]=='B' or arrLine[11]=='T'):
+
+    if len(arrLine) > 4 and (arrLine[11] == 'B' or arrLine[11] == 'T'):
         #print(arrLine[7] + " " + arrLine[8] + " " + arrLine[12])
-        # ファイル名を日付にしたい
-        with open('names1.csv', 'a', newline='', encoding='UTF-8') as f:
+
+        fileName = '{}.csv'.format(today)
+        with open(fileName, 'a', newline='', encoding='UTF-8') as f:
             writer = csv.writer(f)
             writer.writerow(arrLine)
